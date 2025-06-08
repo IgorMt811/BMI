bafd2eabca90af0a1a186781eb7d93aa406987c8
Igor Mitin <igor.mitin19969@gmail.com>
2025-06-08 18:30:10 +0700

    cheange Main,Bmiservice

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate (int weightKg,double heightM){
+    public int calculate(int weightKg, double heightM) {
         double result = weightKg / heightM;
         int roundedResult = (int) result;
         return roundedResult;
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
         BmiService service = new BmiService();
         int weightKg = 86;
-        double heightM =Math.pow(1.87,2);
-        double bmi = service.calculate(weightKg,heightM);
+        double heightM = Math.pow(1.87, 2);
+        double bmi = service.calculate(weightKg, heightM);
         System.out.println("BMI: " + bmi);
     }
 }
